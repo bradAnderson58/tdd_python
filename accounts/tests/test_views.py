d374220f0001b923abd638d362afd69dca042ec6
Brad <brad.anderson58@gmail.com>
2018-05-01 07:47:06 -0400

    authentication backend

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -2,6 +2,7 @@
 from django.test import TestCase
 from unittest.mock import patch
 import accounts.views
+from accounts.models import Token
 
 class SendLoginEmailViewTest(TestCase):
     def test_redirects_to_home_page(self):
@@ -12,7 +13,6 @@ class SendLoginEmailViewTest(TestCase):
 
     @patch('accounts.views.send_mail')
     def test_sends_mail_to_address_from_post(self, mock_send_mail):
-
         self.client.post('/accounts/send_login_email', data={
             'email': 'edith@example.com'
         })
@@ -22,3 +22,39 @@ class SendLoginEmailViewTest(TestCase):
         self.assertEqual(subject, 'Your login link for Superlists')
         self.assertEqual(from_email, 'noreply@superlists')
         self.assertEqual(to_list, ['edith@example.com'])
+
+    @patch('accounts.views.send_mail')
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+
+        token = Token.objects.first()
+        expected_url = f'http://testserver/accounts/login?token={token.uid}'
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertIn(expected_url, body)
+
+    def test_adds_success_message(self):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        }, follow=True)
+
+        message = list(response.context['messages'])[0]
+        self.assertEqual(
+            message.message,
+            "Check your email, we've sent you a link you can use to log in."
+        )
+        self.assertEqual(message.tags, "success")
+
+    def test_creates_token_associated_with_email(self):
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+        token = Token.objects.first()
+        self.assertEqual(token.email, 'edith@example.com')
+
+
+class LoginViewTest(TestCase):
+    def test_redirects_to_home_page(self):
+        response = self.client.get('/accounts/login?token=abc123')
+        self.assertRedirects(response, '/')
