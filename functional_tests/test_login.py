fbacc45fa3d2a4394d1a587bb4a6967091e10b9d
Brad <brad.anderson58@gmail.com>
2018-05-03 07:07:08 -0400

    tests for pre-authenticated users

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -40,9 +40,7 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # she is logged in!
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Log out')
-        )
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(TEST_EMAIL, navbar.text)
 
@@ -50,8 +48,4 @@ class LoginTest(FunctionalTest):
         self.browser.find_element_by_link_text('Log out').click()
 
         # she is logged out
-        self.wait_for(
-            lambda: self.browser.find_element_by_name('email')
-        )
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
