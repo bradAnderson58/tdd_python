5fd608e78c946342d59ca3da99932767cb45a0ed
Brad <brad.anderson58@gmail.com>
2018-05-08 06:49:43 -0400

    add owner field to list model

--- lists/models.py
+++ lists/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 class List(models.Model):
-    
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+
+    @property
+    def name(self):
+        return self.item_set.first().text
+
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
+
 
 class Item(models.Model):
     text = models.TextField(default='')
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 from lists.models import Item, List
+
 
 class ItemModelTest(TestCase):
     def test_default_text(self):
@@ -46,7 +50,22 @@ class ItemModelTest(TestCase):
         item = Item(text='some text')
         self.assertEqual(str(item), 'some text')
 
+
 class ListModelTest(TestCase):
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
+    def test_lists_can_have_owners(self):
+        user = User.objects.create(email='a@b.com')
+        list_ = List.objects.create(owner=user)
+        self.assertIn(list_, user.list_set.all())
+
+    def test_list_owner_is_optional(self):
+        List.objects.create()  # should not raise
+
+    def test_list_name_is_first_item_text(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='first item')
+        Item.objects.create(list=list_, text='second item')
+        self.assertEqual(list_.name, 'first item')
