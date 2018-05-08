eaea3794276fb6381607e45809222a22b75ef820
Brad <brad.anderson58@gmail.com>
2018-05-08 06:39:00 -0400

    new list tries to assign owner but cant

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,11 +2,14 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from django.utils.html import escape
+from django.contrib.auth import get_user_model
 from unittest import skip
 
 from lists.views import home_page
 from lists.models import Item, List
 from lists.forms import ItemForm, ExistingListItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
+User = get_user_model()
+
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -16,6 +19,7 @@ class HomePageTest(TestCase):
     def test_home_page_uses_item_form(self):
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], ItemForm)
+
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -100,6 +104,7 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
 
+
 class NewListTest(TestCase):
     def test_can_save_POST_request(self):
         self.client.post('/lists/new', data={'text': 'A new list item'})
@@ -130,7 +135,22 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
+
 class MyListsTest(TestCase):
     def test_my_lists_url_rendeers_my_lists_template(self):
+        user = User.objects.create(email='a@b.com')
         response = self.client.get('/lists/users/a@b.com/')
         self.assertTemplateUsed(response, 'my_lists.html')
+
+    def test_passes_correct_owner_to_template(self):
+        User.objects.create(email='wrong@owner.com')
+        correct_user = User.objects.create(email='a@b.com')
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertEqual(response.context['owner'], correct_user)
+
+    def test_list_owner_is_saved_if_user_is_authenticated(self):
+        user = User.objects.create(email='a@b.com')
+        self.client.force_login(user)
+        self.client.post('/lists/new', data={'text': 'new item'})
+        list_ = List.objects.first()
+        self.assertEqual(list_.owner, user)
