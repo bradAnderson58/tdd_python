8bd9d2b89c85c2d07a979d983afd5f2e457543d3
Brad <brad.anderson58@gmail.com>
2018-05-17 09:10:47 -0400

    add ft for sharing component

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -6,6 +6,8 @@ from .server_tools import reset_database
 import time
 import os
 from datetime import datetime
+from .management.commands.create_session import create_pre_authenticated_session
+from django.conf import settings
 
 MAX_WAIT = 20
 SCREEN_DUMP_LOCATION = os.path.join(
@@ -43,6 +45,21 @@ class FunctionalTest(StaticLiveServerTestCase):
                 self.dump_html()
         self.browser.quit()
         super().tearDown()
+
+    def create_pre_authenticated_session(self, email):
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+
+        # to set a cookie we need to first visit the domain
+        # 404 pages load the quickest
+        self.browser.get(self.live_server_url + '/404_no_such_url/')
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/'
+        ))
 
     def _test_has_failed(self):
         # slightly obscure but only way
